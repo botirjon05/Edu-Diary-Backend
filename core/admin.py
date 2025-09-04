38765233f23d24f8ed2931e9ed87b0120d802b45
Botirjon Salokhiddinov <salokhiddinovbotirjon@gmail.com>
2025-09-04 23:48:09 +0200

    feat(admin): register Subject

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Subject
 
-# Register your models here.
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "color", "created_at")
+    search_fields = ("code", "name")
