d205bf2f412dcba4fae4b9f6f924d775d03ac0e4
Botirjon Salokhiddinov <salokhiddinovbotirjon@gmail.com>
2025-09-04 23:37:07 +0200

    feat (db): add Subject model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    code = models.CharField(max_length = 10, unique = True)  # MS for Math
+    name = models.CharField(max_length = 100)                # Mathematics
+    color = models.CharField(max_length = 7, blank = True, null = True)  # #22c55e
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.code} - {self..name}"
