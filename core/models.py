315ce6b250222b4b3a46e994bcd5e01bceecf5e4
Botirjon Salokhiddinov <salokhiddinovbotirjon@gmail.com>
2025-09-04 23:41:21 +0200

    chore: run migrations

--- core/models.py
+++ core/models.py
@@ -7,4 +7,4 @@ class Subject(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return f"{self.code} - {self.name}" 
+        return f"{self.code} - {self.name}"
