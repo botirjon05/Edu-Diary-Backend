fb234049097d507b073619305e5fdd9ad57b9849
Botirjon Salokhiddinov <salokhiddinovbotirjon@gmail.com>
2025-09-05 00:01:34 +0200

    feat(api): read-only  /api/subjects/endpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Subject
+from .serializers import SubjectSerializer
 
-# Create your views here.
+class SubjectViewSet (viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.all().order_by("name")
+    serializer_class = SubjectSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+
